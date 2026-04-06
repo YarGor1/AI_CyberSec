ea8b799e0f27b3d090fd9b7fea11bf0938c10b26
YarGor1 <115700127+YarGor1@users.noreply.github.com>
2026-04-06 19:16:26 +0300

    Add files via upload

---
+++ LB2/LB2.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+melbourne_file_path = 'melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+#print (melbourne_data.describe())
+#print(melbourne_data.columns)
+melbourne_data = melbourne_data.dropna(axis=0)
+
+y = melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+#print(X.describe())
+#print(X.head())
+
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+
+#print("Making predictions for the following 5 houses:")
+#print(X.head())
+#print("The predictions are")
+#print(melbourne_model.predict(X.head()))
+
+
+predicted_home_prices = melbourne_model.predict(X)
+#print(mean_absolute_error(y, predicted_home_prices))
+
+
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+melbourne_model = DecisionTreeRegressor()
+melbourne_model.fit(train_X, train_y)
+val_predictions = melbourne_model.predict(val_X)
+#print(mean_absolute_error(val_y, val_predictions))
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds)) 
